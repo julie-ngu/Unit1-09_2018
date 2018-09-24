6f98d9c6267ef658153793e95b4669d9c4241b9c
julie-ngu <31775733+julie-ngu@users.noreply.github.com>
2018-09-24 16:46:13 -0400

    edited code; fixed table

--- ClassMarksExtra.java
+++ ClassMarksExtra.java
@@ -6,7 +6,6 @@
  * This program generates random marks of 6 assignment for 4 students. It has 
  * 		a function that accepts the array of marks and returns the class 
  * 		average. It then prints it in a table.
- * Modified version so that student marks are higher.
  * 
  ****************************************************************************/
 import java.util.Random;
@@ -18,7 +17,7 @@ public class ClassMarksExtra {
 		
 		for(int students = 0; students < classMarksArr.length; students ++) {
 			for(int assignments = 0; assignments < classMarksArr[students].length; assignments++) {
-				int randMark = randGen.nextInt(100-85) + 85;
+				int randMark = randGen.nextInt(100 - 85) + 85;
 				classMarksArr[students][assignments] = randMark;
 			}
 		}
@@ -33,14 +32,19 @@ public class ClassMarksExtra {
 		//prints array in table
 		System.out.print("STUDENTS\t");
 		for(int colNum = 0; colNum < 7; colNum ++) {
-			System.out.print(colNum + "\t");
+			System.out.print(colNum + "   |\t");
 		}
 		
-		System.out.println("\n");
+		System.out.print("\n\t\t");
+		for(int count = 0; count < 53; count ++) {
+			System.out.print("—");
+		}
+		System.out.println("");
+		
 		int rowNum = 1;
 		
 		for(int arrRow = 0; arrRow < arrPrt.length; arrRow ++) {
-			System.out.print("\t\t" + rowNum + "\t");
+			System.out.print("\t\t" + rowNum + "   |\t");
 			rowNum++;
 			for(int arrCol = 0; arrCol < arrPrt[arrRow].length; arrCol ++) {
 				System.out.print(arrPrt[arrRow][arrCol] + "\t");
